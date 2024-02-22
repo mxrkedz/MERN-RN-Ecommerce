f9f78f2b4ca309fce2cff6316599fb3e46f2bee5
EmmFran <emmanuelfrancisco924@gmail.com>
2024-02-22 22:58:51 +0800

    product card

--- frontend/screens/Home.jsx
+++ frontend/screens/Home.jsx
@@ -4,6 +4,8 @@ import { defaultStyle, colors } from '../styles/styles';
 import Header from '../components/Header';
 import { Avatar, Button } from "react-native-paper";
 import SearchModal from '../components/SearchModal';
+import ProductCard from '../components/ProductCard';
+import { useNavigation } from '@react-navigation/native';
 
 const categories = [{ category: "Nice", _id: "asdfasdf" },
   { category: "Nice2", _id: "asdssdfcasdfassdfasdf" },
@@ -12,14 +14,26 @@ const categories = [{ category: "Nice", _id: "asdfasdf" },
   { category: "Nice5", _id: "asdfaqqweqwsdf" }
   ];
 
-const products =[{
+const products =[
+  {
   price: 9999999,
+  stock: 23,
   name: "Gerard",
-  _id: "asdfasdfsa",
+  _id: "asdfasdasdfsa",
   images: [{
     url:"https://scontent.fcrk1-2.fna.fbcdn.net/v/t1.15752-9/426156173_308329602232698_3534526598458257776_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeF_xcK6M3OwRlyAOD3bbViu8pK-IATGxs3ykr4gBMbGzUHuEz5ZmqrL262sGjngZftfioMD6-5HhG-CBX0hl-65&_nc_ohc=gVzKYfcLxdgAX8RWtVU&_nc_ht=scontent.fcrk1-2.fna&oh=03_AdQe61gr7qf8QXQZcjGUQtFnIV9i55PC0zHwjk-We-d2gw&oe=65FEB87E",
   }],
-}];
+  },
+  {
+    price: 9999999,
+    stock: 23,
+    name: "Mark",
+    _id: "sdgsdgsdasd",
+    images: [{
+      url:"https://scontent.fmnl17-3.fna.fbcdn.net/v/t1.15752-9/423454352_3310668132564914_5757140789262203791_n.png?_nc_cat=103&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeEoJFlOQOjelSrlqSd4sy-ux8hBHRgD2y_HyEEdGAPbL-E-MeJ7qV9lH0TtMoqzlHMvrmCl4mJAP47zO8WTErdu&_nc_ohc=g5Bwlm2cp5oAX8jKKoZ&_nc_ht=scontent.fmnl17-3.fna&oh=03_AdTgvnpft9rzcYlBcjpxxq3QYZbkJfD70w_V5hMRAqto-A&oe=65FEBF49"    }],
+  }
+
+];
 
 const Home = () => {
 
@@ -27,9 +41,18 @@ const [category,setCategory] = useState("");
 const [activeSearch, setActiveSearch] = useState(false);
 const [searchQuery, setSearchQuery] = useState("");
 
+const navigate = useNavigation();
+
 const categoryButtonHandler = (id) => {
   setCategory(id);
 };
+
+const addToCartHandler = (id) => {
+  console.log("Add to Cart", id);
+};
+
+
+
 console.log(category);
   return (
     <>
@@ -108,6 +131,25 @@ console.log(category);
       </View>
     {/* products */}
 
+          <View style = {{ flex: 1 }}>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              {
+                products.map((item,index)=>(
+                  <ProductCard
+                    stock={item.stock}
+                    name={item.name}
+                    price={item.price}
+                    image={item.images[0]?.url}
+                    addToCartHandler={addToCartHandler}
+                    id={item._id}
+                    key={item.id}
+                    i={index}
+                    navigate= {navigate}
+                  />
+                ) )
+              }
+            </ScrollView>
+          </View>
 
     </View>
   </>
--- frontend/styles/styles.js
+++ frontend/styles/styles.js
@@ -5,7 +5,7 @@ export const colors={
     color1_light: "rgba(227,25,99)",
     color1_light2: "rgba(199,0,73,0.8)",
     color2: "white",
-    color3: "rgb(45,45,45,)",
+    color3: "rgb(45,45,45)",
     color4: "transparent",
     color5: "#f2f2f2",
     color6: "#f7f7f7",
