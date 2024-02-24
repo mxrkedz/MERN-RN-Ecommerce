a8852d3fd7790e9d3776bf27396cb0dcaec1922a
mxrkedz <pabelicojohnmark@gmail.com>
2024-02-25 05:48:41 +0800

    orderItem

--- frontend/screens/Orders.jsx
+++ frontend/screens/Orders.jsx
@@ -4,6 +4,7 @@ import { colors, defaultStyle, formHeading } from "../styles/styles";
 import Header from "../components/Header";
 import Loader from "../components/Loader";
 import { Headline } from "react-native-paper";
+import OrderItem from "../components/OrderItem";
 
 const orders = [
   {
@@ -12,7 +13,7 @@ const orders = [
       address: "1234 street",
       city: "taguig",
       country: "pilipins",
-      pincode: "1400",
+      pinCode: "1400",
     },
     createdAt: "02-25-2024T2324",
     orderStatus: "Processing",
@@ -25,7 +26,7 @@ const orders = [
       address: "1234 street",
       city: "3331taguig",
       country: "pilipins",
-      pincode: "1400",
+      pinCode: "1400",
     },
     createdAt: "02-25-2024T2324",
     orderStatus: "Processing",
@@ -38,7 +39,7 @@ const orders = [
       address: "1234 street",
       city: "112taguig",
       country: "pilipins",
-      pincode: "1400",
+      pinCode: "1400",
     },
     createdAt: "02-25-2024T2324",
     orderStatus: "Processing",
@@ -61,7 +62,20 @@ const Orders = () => {
       ) : (
         <View style={{ padding: 10, flex: 1 }}>
           <ScrollView showsVerticalScrollIndicator={false}>
-            {orders.length > 0 ? null : (
+            {orders.length > 0 ? (
+              orders.map((item, index) => (
+                <OrderItem
+                  key={item._id}
+                  id={item._id}
+                  i={index}
+                  price={item.totalAmount}
+                  status={item.orderStatus}
+                  paymentMethod={item.paymentMethod}
+                  orderedOn={item.createdAt.split("T")[0]}
+                  address={`${item.shippingInfo.address}, ${item.shippingInfo.city}, ${item.shippingInfo.country}, ${item.shippingInfo.pinCode}`}
+                />
+              ))
+            ) : (
               <Headline style={{ textAlign: "center" }}>
                 No Orders Yet!
               </Headline>
