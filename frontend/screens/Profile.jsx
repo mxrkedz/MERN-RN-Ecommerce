2d997e1ac8b57b302d9198c394e56c1240f5ae77
mxrkedz <pabelicojohnmark@gmail.com>
2024-02-25 04:30:44 +0800

    buttonBox

--- frontend/screens/Profile.jsx
+++ frontend/screens/Profile.jsx
@@ -2,50 +2,84 @@ import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
 import { defaultStyle, formHeading, colors } from "../styles/styles";
 import { Avatar, Button } from "react-native-paper";
+import ButtonBox from "../components/ButtonBox";
+import Footer from "../components/Footer";
 
 const user = {
   name: "Mark",
   email: "mark@gmail.com",
 };
+
+const loading = false;
+
 const Profile = ({ navigation }) => {
   const { avatar, setAvatar } = useState(null);
+
+  const navigateHandler = () => {};
+
   return (
-    <View style={defaultStyle}>
-      <View style={{ marginBottom: 20 }}>
-        <Text style={formHeading}>Profile</Text>
+    <>
+      <View style={defaultStyle}>
+        <View style={{ marginBottom: 20 }}>
+          <Text style={formHeading}>Profile</Text>
+        </View>
+
+        {/* Loading */}
+        
+        <View style={styles.container}>
+          <Avatar.Image
+            source={{ uri: avatar }}
+            size={100}
+            style={{ backgroundColor: colors.color1 }}
+          />
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate("camera", { updateProfile: true })
+            }
+          >
+            <Button textColor={colors.color1}>Change Photo</Button>
+          </TouchableOpacity>
+
+          <Text style={styles.name}>{user?.name}</Text>
+          <Text
+            style={{
+              fontWeight: 300,
+              color: colors.color2,
+            }}
+          >
+            {user?.email}
+          </Text>
+        </View>
+        <View>
+          <View
+            style={{
+              flexDirection: "row",
+              margin: 10,
+              justifyContent: "space-between",
+            }}
+          >
+            <ButtonBox
+              handler={navigateHandler}
+              text={"Orders"}
+              icon={"format-list-bulleted-square"}
+            />
+            <ButtonBox icon={"view-dashboard"} text={"Admin"} reverse={true} />
+            <ButtonBox text={"Profile"} icon={"pencil"} />
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              margin: 10,
+              justifyContent: "space-evenly",
+            }}
+          >
+            <ButtonBox text={"Password"} icon={"pencil"} />
+            <ButtonBox text={"Sign Out"} icon={"exit-to-app"} />
+          </View>
+        </View>
       </View>
-
-      <View style={styles.container}>
-        <Avatar.Image
-          source={{ uri: avatar }}
-          size={100}
-          style={{ backgroundColor: colors.color1 }}
-        />
-        <TouchableOpacity
-          onPress={() => navigation.navigate("camera", { updateProfile: true })}
-        >
-          <Button textColor={colors.color1}>Change Photo</Button>
-        </TouchableOpacity>
-
-        <Text style={styles.name}>{user?.name}</Text>
-        <Text
-          style={{
-            fontWeight: 300,
-            color: colors.color2,
-          }}
-        >
-          {user?.email}
-        </Text>
-      </View>
-
-      <View
-        style={{
-          flexDirection: "row",
-          margin: 10,
-          justifyContent: "space-between",
-        }}
-      ></View>
-    </View>
+      <Footer />
+    </>
   );
 };
 
