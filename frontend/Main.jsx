4227bc1efa0d8e1800a57f0cf95ffa019a880e0a
EmmFran <emmanuelfrancisco924@gmail.com>
2024-03-03 22:07:53 +0800

    NewProducts

--- frontend/Main.jsx
+++ frontend/Main.jsx
@@ -19,6 +19,7 @@ import AdminDashboard from "./screens/Admin/AdminDashboard";
 import Categories from "./screens/Admin/Categories";
 import AdminOrders from "./screens/Admin/AdminOrders";
 import UpdateProduct from "./screens/Admin/UpdateProduct";
+import NewProduct from "./screens/Admin/NewProduct";
 
 const Stack = createNativeStackNavigator();
 
@@ -53,6 +54,7 @@ const Main = () => {
       <Stack.Screen name="categories" component={Categories} />
       <Stack.Screen name="adminorders" component={AdminOrders} />
       <Stack.Screen name="updateproduct" component={UpdateProduct} />
+      <Stack.Screen name="newproduct" component={NewProduct} />
 
         </Stack.Group>
       </Stack.Navigator>
