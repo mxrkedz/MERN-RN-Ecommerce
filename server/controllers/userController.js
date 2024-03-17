9b6168170361b7ac1c028647ba6719163498fcb9
unknown <gerardjames.meneses@tup.edu.ph>
2024-03-17 16:14:52 +0800

    backend authentication

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,7 @@
 import { asyncError } from "../middlewares/error.js";
 import {User} from "../models/user.js"
 import ErrorHandler from "../utils/error.js";
-import { sendToken } from "../utils/features.js";
+import { cookieOptions, sendToken } from "../utils/features.js";
 
 
 export const login = asyncError(async  (req,res,next) => {
@@ -46,12 +46,27 @@ export const register = asyncError(async (req,res,next) => {
 
 });
 
+export const logOut = asyncError(async (req,res,next)=>{ 
+
+
+  res.status(200).cookie("token", "",{
+    ...cookieOptions,
+    expires: new Date(Date.now()),
+  }).json({
+    
+    success: true, 
+    message:"Logout Successfully",
+  });
+
+});
+
+
 export const getMyProfile = asyncError(async (req,res,next)=>{ 
   const user = await User.findById(req.user._id);
 
 
   res.status(200).json({
     success: true, user, 
-  })
+  });
 
-})
+});
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getMyProfile, login, register } from "../controllers/userController.js";
+import { getMyProfile, logOut, login, register } from "../controllers/userController.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
@@ -9,5 +9,7 @@ router.post("/login", login);
 router.post("/register", register);
 
 router.post("/me", isAuthenticated, getMyProfile);
+
+router.post("/logout", isAuthenticated, logOut);
 
 export default router;
