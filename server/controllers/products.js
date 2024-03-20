e055a55bdb75472f89c266c458aa70cd70534646
EmmFran <emmanuelfrancisco924@gmail.com>
2024-03-21 02:49:52 +0800

    delete products & category CRUD

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -3,6 +3,7 @@ import { Product } from "../models/product.js";
 import ErrorHandler from "../utils/error.js";
 import cloudinary from "cloudinary";
 import { getDataUri } from "../utils/features.js";
+import { Category } from "../models/category.js";
 
 export const getAllProducts = asyncError (async (req, res, next) => {
     const products = await Product.find({});
@@ -88,3 +89,86 @@ export const addProductImage = asyncError (async (req, res, next) => {
         message:"Image Added Succesfully",
     });
 });
+
+export const deleteProductImage = asyncError(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product) return next(new ErrorHandler("Product not found", 404));
+  
+    const id = req.query.id;
+  
+    if (!id) return next(new ErrorHandler("Please Image Id", 400));
+  
+    let isExist = -1;
+  
+    product.images.forEach((item, index) => {
+      if (item._id.toString() === id.toString()) isExist = index;
+    });
+  
+    if (isExist < 0) return next(new ErrorHandler("Image doesn't exist", 400));
+  
+    await cloudinary.v2.uploader.destroy(product.images[isExist].public_id);
+  
+    product.images.splice(isExist, 1);
+  
+    await product.save();
+  
+    res.status(200).json({
+      success: true,
+      message: "Image Deleted Successfully",
+    });
+  });
+
+  export const deleteProduct = asyncError(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product) return next(new ErrorHandler("Product not found", 404));
+  
+    for (let index = 0; index < product.images.length; index++) {
+      await cloudinary.v2.uploader.destroy(product.images[index].public_id);
+    }
+    await product.deleteOne();
+
+    res.status(200).json({
+      success: true,
+      message: "Product Deleted Successfully",
+    });
+  });
+
+  export const addCategory = asyncError(async (req, res, next) => {
+    await Category.create(req.body);
+  
+    res.status(201).json({
+      success: true,
+      message: "Category Added Successfully",
+    });
+  });
+  
+  export const getAllCategories = asyncError(async (req, res, next) => {
+    const categories = await Category.find({});
+  
+    res.status(200).json({
+      success: true,
+      categories,
+    });
+  });
+  
+  export const deleteCategory = asyncError(async (req, res, next) => {
+    const category = await Category.findById(req.params.id);
+    if (!category) return next(new ErrorHandler("Category Not Found", 404));
+    const products = await Product.find({ category: category._id });
+  
+    for (let i = 0; i < products.length; i++) {
+      const product = products[i];
+      product.category = undefined;
+      await product.save();
+    }
+  
+    await category.deleteOne();
+  
+    res.status(200).json({
+      success: true,
+      message: "Category Deleted Successfully",
+    });
+  });
+
+
+
--- server/routes/product.js
+++ server/routes/product.js
@@ -1,16 +1,27 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
 import { singleUpload } from "../middlewares/multer.js";
-import { addProductImage, createProduct, getAllProducts, getProductDetails, updateProduct } from "../controllers/products.js";
+import { addCategory, addProductImage, createProduct, deleteCategory, deleteProduct, deleteProductImage, getAllCategories, getAllProducts, getProductDetails, updateProduct } from "../controllers/products.js";
 
 const router = express.Router();
 
 router.get("/all", getAllProducts);
 
-router.route("/single/:id").get(getProductDetails).put(isAuthenticated, updateProduct);
+router.route("/single/:id")
+.get(getProductDetails)
+.put(isAuthenticated, updateProduct)
+.delete(isAuthenticated, deleteProduct);;
 
 router.post("/new", isAuthenticated, singleUpload, createProduct);
 
-router.route("/images/:id").post(isAuthenticated, singleUpload, addProductImage);
+router.route("/images/:id")
+.post(isAuthenticated, singleUpload, addProductImage)
+.delete(isAuthenticated, deleteProductImage);
+
+router.post("/category", isAuthenticated, addCategory);
+
+router.get("/categories", getAllCategories);
+
+router.delete("/category/:id", isAuthenticated,  deleteCategory);
 
 export default router;
