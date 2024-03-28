ac21ed2b933f7b279c1e5b8cffcca8075026b2a9
mxrkedz <pabelicojohnmark@gmail.com>
2024-03-28 21:06:12 +0800

    emailTransaction

--- frontend/redux/store.js
+++ frontend/redux/store.js
@@ -4,14 +4,14 @@ import { otherReducer } from "./reducers/otherReducer";
 import { productReducer } from "./reducers/productReducer";
 import { cartReducer } from "./reducers/cartReducer";
 
-
 export const store = configureStore({
-    reducer: {
-        user:userReducer,
-        other: otherReducer,
-        product: productReducer,
-        cart: cartReducer
-    },
-  });
+  reducer: {
+    user: userReducer,
+    other: otherReducer,
+    product: productReducer,
+    cart: cartReducer,
+  },
+});
 
-export const server = "https://mobileproj-server.onrender.com/api/v1"
+// export const server = "https://mobileproj-server.onrender.com/api/v1";
+export const server = "http://192.168.0.111:5000/api/v1";
